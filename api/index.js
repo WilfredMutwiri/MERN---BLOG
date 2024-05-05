2c1c25f2a4735bfe9534a203dbc3345bbcb036f6
Wilfred Mutwiri <Wilfred Mutwiri>
2024-05-06 00:20:58 +0300

    Connect to the database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config()
 const app=express();
-app.listen(3000,()=>{
-    console.log("Listening on port 3000");
+app.listen(process.env.PORT,()=>{
+    console.log(`Listening on port ${process.env.PORT}`);
 })
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        console.log("Connected to db successfully!");
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
