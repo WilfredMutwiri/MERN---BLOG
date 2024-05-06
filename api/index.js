74e524ce405252f3c0134bf389f1912ec76f2b3f
Wilfred Mutwiri <Wilfred Mutwiri>
2024-05-06 16:28:49 +0300

    create signup api

--- api/index.js
+++ api/index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.routes.js';
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config()
 const app=express();
+app.use(express.json())
 app.listen(process.env.PORT,()=>{
     console.log(`Listening on port ${process.env.PORT}`);
 })
@@ -17,3 +19,4 @@ mongoose.connect(process.env.MONGO_URI)
     })
 // test api
 app.use('/api/user',userRoutes)
+app.use('/api/auth',authRoutes)
