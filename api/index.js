7d52b18e8c4fe3b6c04fe8faa7f5bb6bcab318c3
Wilfred Mutwiri <Wilfred Mutwiri>
2024-05-06 08:48:23 +0300

    create a test API route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.routes.js';
 
 dotenv.config()
 const app=express();
@@ -14,3 +15,5 @@ mongoose.connect(process.env.MONGO_URI)
     .catch((error)=>{
         console.log(error);
     })
+// test api
+app.use('/api/user',userRoutes)
