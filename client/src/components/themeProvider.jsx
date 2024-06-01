4c8581907e98ec350390d8c9c3970885d80976bd
Wilfred Mutwiri <Wilfred Mutwiri>
2024-06-01 23:13:19 +0300

    Add image upload

--- client/src/components/themeProvider.jsx
+++ client/src/components/themeProvider.jsx
@@ -5,7 +5,7 @@ export default function ThemeProvider({children}) {
     const {theme}=useSelector(state=>state.theme)
   return (
     <div className={theme}>
-        <div className="bg-white text-gray-700 dark:text-gray-200 dark:bg-gray-950">
+        <div className="bg-white text-gray-700 dark:text-gray-200 dark:bg-gray-950 min-h-screen">
         {children}
         </div>
     </div>
